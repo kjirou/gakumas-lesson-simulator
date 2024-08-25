786621f81f797edc2494b5529d032210f47a4e47
kjirou <kjirou.web@gmail.com>
2024-08-25 12:33:16 +0900

    ダイアログの背景へ着色する

--- src/components/IndexPage/DescriptionDialog.tsx
+++ src/components/IndexPage/DescriptionDialog.tsx
@@ -5,24 +5,27 @@ const DescriptionDialogRaw: React.FC<{
   onClickBackdrop?: () => void;
 }> = (props) => {
   return (
-    <div
-      className="absolute w-full h-full top-0 left-0 z-20 flex justify-center items-center"
-      onClick={(event: React.MouseEvent<HTMLElement>) => {
-        event.stopPropagation();
-        if (props.onClickBackdrop) {
-          props.onClickBackdrop();
-        }
-      }}
-    >
+    <>
       <div
-        className="w-10/12 p-1 text-sm border bg-white"
+        className="absolute w-full h-full top-0 left-0 z-30 flex justify-center items-center"
         onClick={(event: React.MouseEvent<HTMLElement>) => {
           event.stopPropagation();
+          if (props.onClickBackdrop) {
+            props.onClickBackdrop();
+          }
         }}
       >
-        {props.children}
+        <div
+          className="w-10/12 p-1 text-sm border bg-white"
+          onClick={(event: React.MouseEvent<HTMLElement>) => {
+            event.stopPropagation();
+          }}
+        >
+          {props.children}
+        </div>
       </div>
-    </div>
+      <div className="absolute w-full h-full top-0 left-0 z-20 bg-black opacity-5" />
+    </>
   );
 };
 
